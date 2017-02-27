2e29233159b8c1b8427f8ffdceb1a399b68c7f8e
gregbonner <greg.d.bonner@gmail.com>
2017-02-26 18:56:25 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+/**
+ * promptForTipPercent - description
+ *
+ * @return {type}  description
+ */
+function promptForTipPercent() {
+
+}
+
+
+/**
+ * computeTip - description
+ *
+ * @param  {type} billAmount description
+ * @param  {type} tipPercent description
+ * @return {type}            description
+ */
+function computeTip(billAmount, tipPercent) {
+  var tipAmount;
+  var totalAmount;
+
+  tipAmount = billAmount * tipPercent;
+  totalAmount = billAmount + tipAmount;
+  return totalAmount;
+}
+
+/**
+ * displayTotals - description
+ *
+ * @return {type}  description
+ *
+ */
+function displayTotals() {
+
+}
